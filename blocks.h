963c00dd760c7113675c8d74dfbcbcf42e7c8a14
Baseng0815 <1shedex2@gmail.com>
2020-09-26 03:38:14 +0200

    change block order

--- blocks.h
+++ blocks.h
@@ -1,10 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/	/*Command*/	        /*Update Interval*/	/*Update Signal*/
-    {"",        "get-memory",	        5,		        0},
-    {"",        "get-date",	        15,		        0},
+    {"",        "get-memory",	        1,		        0},
     {"",        "get-volume",           3600,		        9},
-    {"",        "get-weather",          60,		        0}
+    {"",        "get-battery",          60,		        10},
+    {"",        "get-weather",          60,		        0},
+    {"",        "get-date",	        15,		        0}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
