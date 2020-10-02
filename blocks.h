d795e18373145bdabb5d99df48a57462170cdcf6
Baseng0815 <1shedex2@gmail.com>
2020-10-03 00:57:26 +0200

    add kblayout

--- blocks.h
+++ blocks.h
@@ -5,6 +5,7 @@ static const Block blocks[] = {
     {"",        "get-memory",	        1,		        8},
     {"",        "get-volume",           3600,		        9},
     {"",        "get-battery",          60,		        10},
+    {"",        "kblayout get",         3600,		        11},
     {"",        "get-conn",             60,		        8},
     {"",        "get-weather",          60,		        8},
     {"",        "get-date",	        15,		        8}
